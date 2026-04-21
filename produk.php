d4c258e12dd744b4d42c2bcf8c05a9e541fc4205
WhtTheHel <karmanuserbot@gmail.com>
2026-04-21 17:40:26 +0700

    Refactor produk.php for improved structure and script inclusion

--- produk.php
+++ produk.php
@@ -3,7 +3,8 @@
 <html>
 <head>
 <title>Produk Kopi</title>
-
+  
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
 <style>

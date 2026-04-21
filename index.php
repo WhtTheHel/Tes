31a3c1de89fb04d367f689b2f89a4cf060f1956b
WhtTheHel <karmanuserbot@gmail.com>
2026-04-21 17:37:22 +0700

    Fix HTML formatting and improve styles

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <style>
         .hero-section {
             background: linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), 
-                        /*url('img/hero-kopi.jpg'); /* Opsional: Tambahkan background gambar kopi */
+                        /*url('img/hero-kopi.jpg');Opsional */
             background-size: cover;
             background-position: center;
             color: white;
